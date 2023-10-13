ab5b737f7f910a55d7ee6fa9657a092ec9ac4a9c
Dan <daramirezp1112@gmail.com>
2023-10-13 09:46:36 -0500

    hpta input del orto

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import {getCharacters, getEpisodes} from "./data/dataFetch";
 
 class AppContainer extends HTMLElement {
 
+  allcards: HTMLDivElement[] = [];
+
     constructor(){
         super();
         this.attachShadow({mode: "open"}); 
@@ -12,35 +14,67 @@ class AppContainer extends HTMLElement {
 
     async connectedCallback(){
       const episodes = await getEpisodes();
-      this.render(episodes.results);
+      this.render()
+      const button = this.shadowRoot?.querySelector(".btnclick");
+      const input = this.shadowRoot?.querySelector("input");
 
+      button?.addEventListener("click", () => {
+        let value = input?.value
+        console.log(value);
+        this.filter(Number(value), episodes.results)
+      })
+      
     }
 
-    render(episodesData:any){
+    filter(value: number, results: any[]){
+      for (let i = 0 ; i < value ; i++){
+        console.log(results[i])
+        const div = this.ownerDocument.createElement("div") as HTMLDivElement;
+          const newEpisode = this.ownerDocument.createElement("my-card") as Card;
+          newEpisode.setAttribute(Attribute.name, results[i].name);
+          newEpisode.setAttribute(Attribute.ep, results[i].episode);
+          div.appendChild(newEpisode)
+    
+          results[i].characters.forEach(async (character: string) => {
+              const characterData = await getCharacters(character);
+              const newCharacter = this.ownerDocument.createElement("my-char") as Character;
+              newCharacter.setAttribute(AttributeChar.img, characterData.image);
+              newCharacter.setAttribute(AttributeChar.name, characterData.name);
+              div.appendChild(newCharacter);
+          })
+          this.allcards?.push(div);
+      }
+      this.render();
+    }
+
+
+    render(){
       if(this.shadowRoot){
         this.shadowRoot.innerHTML=`
-        
+        <input class="" type="text" placeholder="Escribe un número">
+        <button class="btnclick">Filtrar</button>
+        <p>hpta</p>
         `
 
-      episodesData.forEach((ep: any) => {
-        const div = this.ownerDocument.createElement("div") as HTMLDivElement;
-        const newEpisode = this.ownerDocument.createElement("my-card") as Card;
-        newEpisode.setAttribute(Attribute.name, ep.name);
-        newEpisode.setAttribute(Attribute.ep, ep.episode);
-        div.appendChild(newEpisode)
-
-        ep.characters.forEach(async (character: string) => {
-            const characterData = await getCharacters(character);
-            const newCharacter = this.ownerDocument.createElement("my-char") as Character;
-            newCharacter.setAttribute(AttributeChar.img, characterData.image);
-            newCharacter.setAttribute(AttributeChar.name, characterData.name);
-            console.log(characterData.url)
-            div.appendChild(newCharacter);
+        this.allcards.forEach((c) => {
+          this.shadowRoot?.appendChild(c)
         })
-        this.shadowRoot?.appendChild(div);
-      })
-
-        
+        /*episodesData.forEach((ep: any) => {
+          const div = this.ownerDocument.createElement("div") as HTMLDivElement;
+          const newEpisode = this.ownerDocument.createElement("my-card") as Card;
+          newEpisode.setAttribute(Attribute.name, ep.name);
+          newEpisode.setAttribute(Attribute.ep, ep.episode);
+          div.appendChild(newEpisode)
+    
+          ep.characters.forEach(async (character: string) => {
+              const characterData = await getCharacters(character);
+              const newCharacter = this.ownerDocument.createElement("my-char") as Character;
+              newCharacter.setAttribute(AttributeChar.img, characterData.image);
+              newCharacter.setAttribute(AttributeChar.name, characterData.name);
+              div.appendChild(newCharacter);
+          })
+          this.shadowRoot?.appendChild(div);
+        })*/
     }
 }
 
